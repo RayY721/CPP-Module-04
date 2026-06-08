ff4152df016e65b3b4384e29d17134f2264420e1
kuyu <kuyu@student.codam.nl>
2026-06-08 17:30:28 +0200

    Working on ex00.

---
+++ ex00/Animal.hpp
@@ -0,0 +1,20 @@
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <string>
+class Animal
+{
+	protected:
+		std::string type;
+
+	public:
+			Animal();
+			Animal(const Animal& other);
+			Animal& operator=(const Animal& other);
+			~Animal();
+
+			std::string getType() const;
+			void		makeSound() const;
+};
+
+#endif
