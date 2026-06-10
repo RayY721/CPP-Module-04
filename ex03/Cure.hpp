e17c5d0a72a9ee38426bc923c6977f46ad2b3d84
kuyu <kuyu@student.codam.nl>
2026-06-10 17:02:18 +0200

    Working on ex03, don't really get it.

---
+++ ex03/Cure.hpp
@@ -0,0 +1,20 @@
+
+#ifndef CURE_HPP
+# define CURE_HPP
+
+# include "AMateria.hpp"
+# include "ICharacter.hpp"
+
+class Cure : public AMateria
+{
+    public:
+        Cure();
+        Cure(Cure const& other);
+        Cure& operator=(Cure const& other);
+        ~Cure();
+
+        AMateria*   clone() const;
+        void        use(Icharacter& target);
+};
+
+#endif
